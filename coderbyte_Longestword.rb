ca9cb9c68370b9136b8eafd377701551b724dd66
Hyung Kyu <simpson1757@gmail.com>
2015-07-31 20:55:22 +0900

    Create coderbyte_Longestword.rb

---
+++ coderbyte_Longestword.rb
@@ -0,0 +1,19 @@
+def LongestWord(sen)
+
+longest = ""
+
+  sen.scan(/\w+/) do |word|
+    if word.length > longest.length
+      longest = word
+    end
+  end
+                      
+  return longest
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+LongestWord(STDIN.gets)  
+
+
